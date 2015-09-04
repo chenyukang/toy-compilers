f3ce5d5c28454f95f472028aea0d9c033075a3db
Yukang <moorekang@gmail.com>
2015-09-05 02:42:34 +0800

    more on white

--- make.sh
+++ make.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 ocamlfind ocamlc -package js_of_ocaml -thread -syntax camlp4o -package js_of_ocaml.syntax -linkpkg -o brain.byte brain.ml
-
 js_of_ocaml brain.byte -o sites/brain.js
+
+
+ocamlfind ocamlc -package js_of_ocaml -thread -syntax camlp4o -package js_of_ocaml.syntax -linkpkg -o white.byte white.ml
+js_of_ocaml white.byte -o sites/white.js
